4ff148da5b8791f874946f96b2413cc319b7b892
jvgoverna <jv11governa2018@gmail.com>
2023-10-16 01:14:56 -0300

    Adicionado Item Sobre

--- Menu.c
+++ Menu.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "Sobre.c"
 #include <ctype.h>
 
 
@@ -8,42 +9,48 @@ int main(){
     char option [100];
     while(1){
         printf("------------------------------\n");
-        printf("0 - Sair\n");
         printf("1 - Cadastrar novo Paciente\n");
         printf("2 - Consultar paciente Cadastrado\n");
         printf("3 - Mostrar Lista Completa\n");
         printf("4 - Atualizar dados do Paciente\n");
         printf("5 - Remover Paciente\n");
+        printf("6 - Sobre\n");
+        printf("7 - Sair\n");
         printf("------------------------------\n");
 
         fgets(option,sizeof(option),stdin);
 
         if(isdigit(option[0])){
             switch (option[0]){
-                case '0':
-                    puts("Saindo");
-                    break;
                 case '1':
-                    puts("----- Opção 1 -----");
+                    puts("---------- Opção 1 ----------");
                     break;
                 case '2':
-                    puts("----- Opção 2 -----");
+                    puts("---------- Opção 2 ----------");
                     break;
                 case '3':
-                    puts("----- Opção 3 -----");
+                    puts("---------- Opção 3 ----------");
                     break;
                 case '4':
-                    puts("----- Opção 4 -----");
+                    puts("---------- Opção 4 ----------");
                     break;
                 case '5':
-                    puts("----- Opção 5 -----");
+                    puts("---------- Opção 5 ----------");
+                    break;
+                case '6':
+                    puts("---------- Opção 6 ----------");
+                    sobre();
+                    break;
+                case '7':
+                    puts("Saindo...");
+                    return 0;
                     break;
                 default:
-                    printf("ERRO Digite um número válido!");
+                    puts("ERRO Digite uma opção válida!");
                     break;
             }
         }else{
-            printf("ERRO Digite um número válido!");
+            puts("ERRO Digite um número válido!");
         }
     }
     return 0;
---
+++ Sobre.c
@@ -0,0 +1,10 @@
+#include "Sobre.h"
+void sobre(){
+    puts("---------- Integrantes ----------");
+    puts("João Vitor Governatore RA.: 24.122.027-6");
+    puts("Pedro Henrique Lega Kramer Costa RA.: 24.122.049-0");
+    puts("---------- 4º Semestre ----------");
+    puts("---------- Diciplina: CC4652 - Estrutura de Dados ----------");
+    puts("---------- 16/10/2023 ----------");
+
+}
